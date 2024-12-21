a4e5b3fb681e16926c28a2b1517eb6c2a5f6102a
Rokas <rokas.ramanauskas0@gmail.com>
2024-12-21 19:19:26 +0200

    www

--- main.java
+++ main.java
@@ -19,7 +19,7 @@ public class main {
         System.out.println();
         int min = 100000, max = 999999;
 
-        int ggg = 54;
+        int ggg = 554;
 
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
